a4185fe76462cb5706777137c6979b6a4924150a
Martino Ferrari <manda.mgf@gmail.com>
2017-05-01 22:42:00 +0200

    Added structure for men utilities

---
+++ mumen/men.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+"""MEN Utilities
+This module contains many utilities to load, creante and store MEN Files:
+https://staff.fnwi.uva.nl/e.bruni/MEN.
+"""
+
+
+def load(path: str):
+    """Function to load a MEN FILE
+
+    Args:
+        path: path to the MEN file to load.
+
+    Returns:
+        the MEN Structure.
+    """
+    # TODO implement
+    return []
+
+
+def store(men: list, path: str):
+    """Function to sture a MEN list to file.
+
+    Args:
+        param men: men list structure to save.
+        param path: path to the file where to store the MEN.
+    """
+    # TODO implement
+    return None
