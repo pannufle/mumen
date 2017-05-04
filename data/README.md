4593f51afe114244028272136747a1a47103fb98
Martino Ferrari <manda.mgf@gmail.com>
2017-05-04 13:44:18 +0200

    removed data folder

--- data/README.md
+++
@@ -1,4 +0,0 @@
-Folder to store the **MEN** data files. To download the resources:
-```bash
-$ python3 download.py
-```
--- mumen/men.py
+++ mumen/men.py
@@ -3,12 +3,12 @@
 This module contains many utilities to load, creante and store MEN Files:
 https://staff.fnwi.uva.nl/e.bruni/MEN.
 """
-import requests
-import zipfile
 import os
+import zipfile
+import requests
 
 
-def parse_men_row(row: str) -> dict:
+def parse_men_row(row):
     """Function to parse a MEN row
     Args:
         row: input row in form of "WORD_L WORD_R SIMILARITY"
@@ -28,13 +28,19 @@ def parse_men_row(row: str) -> dict:
     }
 
 
-def encode_men_row(entry: dict) -> str:
+def encode_men_row(entry):
+    """Function to encode an MEN entry to string
+    Args:
+        entry: MEN entry in form of dictionary
+    Returns:
+        MEN entry string
+    """
     return '{} {} {}'.format(entry["word_L"],
                              entry["word_R"],
                              entry["similarity"])
 
 
-def downlaod_men() -> list:
+def downlaod_men():
     """Download the last version of the MEN data-set from:
     https://staff.fnwi.uva.nl/e.bruni/resources
 
@@ -42,26 +48,27 @@ def downlaod_men() -> list:
         The MEN structure
     """
     reponse = requests.get(
-                "https://staff.fnwi.uva.nl/e.bruni/resources/MEN.zip",
-                stream=True)
+                        "https://staff.fnwi.uva.nl/e.bruni/resources/MEN.zip",
+                        stream=True)
     if reponse.status_code == 200:
         if not os.path.isdir(".tmp/"):
             os.mkdir(".tmp")
-        with open(".tmp/MEN.zip", 'wb') as f:
-            for chunk in reponse:
-                f.write(chunk)
-        with zipfile.ZipFile(".tmp/MEN.zip", "r") as zip_ref:
-            zip_ref.extractall(".tmp/")
+        if not os.path.isdir(".tmp/MEN/"):
+            with open(".tmp/MEN.zip", 'wb') as download:
+                for chunk in reponse:
+                    download.write(chunk)
+            with zipfile.ZipFile(".tmp/MEN.zip", "r") as zip_ref:
+                zip_ref.extractall(".tmp/")
 
-        with open(".tmp/MEN/MEN_dataset_natural_form_full", "r") as f:
-            for men_row in f:
-                yield(parse_men_row(men_row))
+        with open(".tmp/MEN/MEN_dataset_natural_form_full", "r") as men_file:
+            for men_row in men_file:
+                yield parse_men_row(men_row)
     else:
         raise Exception("Download failed ({}) : {}".format(reponse.status_code,
                                                            reponse.text))
 
 
-def load(path: str) -> list:
+def load(path):
     """Function to load a MEN FILE
 
     Args:
@@ -70,24 +77,23 @@ def load(path: str) -> list:
     Returns:
         the MEN Structure.
     """
-    with open(path, "r") as f:
-        for men_row in f:
+    with open(path, "r") as men_file:
+        for men_row in men_file:
             yield parse_men_row(men_row)
 
 
-def store(men: list, path: str):
+def store(men, path):
     """Function to sture a MEN list to file.
 
     Args:
         param men: men list structure to save.
         param path: path to the file where to store the MEN.
     """
-    with open(path, "w") as f:
+    with open(path, "w") as men_file:
         for entry in men:
-            f.write(f'{encode_men_row(entry)}\n')
+            men_file.write('{}\n'.format(encode_men_row(entry)))
 
 
 if __name__ == "__main__":
-    """Simple demo to test MEN functionality"""
-    for entry in downlaod_men():
-        print(entry)
+    for men_entry in downlaod_men():
+        print(men_entry)
