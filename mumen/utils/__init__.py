b86183c867280bece2bcb615912748fce6d5d10f
Martino Ferrari <manda.mgf@gmail.com>
2017-05-04 20:03:09 +0200

    Added utilities folder for download and other future helpers

---
+++ mumen/utils/__init__.py
@@ -0,0 +1,9 @@
+"""How to write meaningful docstrings.
+
+Check out:
+http://stackoverflow.com/questions/
+2557110/what-to-put-in-a-python-module-docstring
+and
+http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+"""
