153bb454b20d75e1191e764b1d583f6e1485bc03
Martino Ferrari <manda.mgf@gmail.com>
2017-05-04 11:32:54 +0200

    First attempt to read MEN files and resources

--- mumen/men.py
+++ mumen/men.py
@@ -3,9 +3,46 @@
 This module contains many utilities to load, creante and store MEN Files:
 https://staff.fnwi.uva.nl/e.bruni/MEN.
 """
+import requests
+import zipfile
+import os
 
 
-def load(path: str):
+def downlaod_men():
+    """Download the last version of the MEN data-set from:
+    https://staff.fnwi.uva.nl/e.bruni/resources
+
+    Returns:
+        The MEN structure
+    """
+    reponse = requests.get(
+                "https://staff.fnwi.uva.nl/e.bruni/resources/MEN.zip",
+                stream=True)
+    if reponse.status_code == 200:
+        if not os.path.isdir(".tmp/"):
+            os.mkdir(".tmp")
+        with open(".tmp/MEN.zip", 'wb') as f:
+            for chunk in reponse:
+                f.write(chunk)
+        with zipfile.ZipFile(".tmp/MEN.zip", "r") as zip_ref:
+            zip_ref.extractall(".tmp/")
+        men = []
+        with open(".tmp/MEN/MEN_dataset_natural_form_full", "r") as f:
+            for men_row in f:
+                men_row = men_row.strip().split()
+                men_entry = {
+                    "word_L": men_row[0],
+                    "word_R": men_row[1],
+                    "similarity": float(men_row[2])
+                }
+                men.append(men_entry)
+        return men
+    else:
+        raise Exception("Download failed ({}) : {}".format(reponse.status_code,
+                                                           reponse.text))
+
+
+def load(path: str) -> list:
     """Function to load a MEN FILE
 
     Args:
@@ -14,8 +51,17 @@ def load(path: str):
     Returns:
         the MEN Structure.
     """
-    # TODO implement
-    return []
+    men = []
+    with open(path, "r") as f:
+        for men_row in f:
+            men_row = men_row.strip().split()
+            men_entry = {
+                "word_L": men_row[0],
+                "word_R": men_row[1],
+                "similarity": float(men_row[2])
+            }
+            men.append(men_entry)
+    return men
 
 
 def store(men: list, path: str):
@@ -27,3 +73,7 @@ def store(men: list, path: str):
     """
     # TODO implement
     return None
+
+
+if __name__ == "__main__":
+    print(downlaod_men())
