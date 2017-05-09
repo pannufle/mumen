a020ccd58fdabd80d435b8a31d43b56c188665f3
Martino Ferrari <manda.mgf@gmail.com>
2017-05-09 10:48:30 +0200

    First try to translate pipeline

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ import logging
 from mumen.constants import CONFIG_PATH
 from mumen.validation.validator import load_yml, validate_yml
 from mumen.men.men import men_pipeline
+from mumen.dictionaries.translate import translate
+from mumen.exceptions.translation import TException
 
 if __name__ == "__main__":
     LOGGER = logging.getLogger("MUMEN")
     LOGGER.setLevel(logging.DEBUG)
     logging.basicConfig(stream=sys.stdout)
 
-    for men in men_pipeline(validate_yml(load_yml(CONFIG_PATH))):
-        LOGGER.info(men)
+    CONFIG = validate_yml(load_yml(CONFIG_PATH))
+    try:
+        for men in translate(men_pipeline(CONFIG), CONFIG):
+            LOGGER.info(men)
+    except TException as exc:
+        LOGGER.error(exc)
