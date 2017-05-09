a1a442a464f7e13121de447d34760e49822e793b
Martino Ferrari <manda.mgf@gmail.com>
2017-05-09 10:18:27 +0200

    First attempt of pipeline and use of logging

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ Mumen read and translate MEN files and then use it to generate YACAB questions.
 author: Martino Ferrari
 author_email: martino.ferrari@etu.unige.ch
 """
+import sys
+import logging
 from mumen.constants import CONFIG_PATH
 from mumen.validation.validator import load_yml, validate_yml
+from mumen.men.men import men_pipeline
 
 if __name__ == "__main__":
-    # TODO implement pipeline
-    validate_yml(load_yml(CONFIG_PATH))
+    LOGGER = logging.getLogger("MUMEN")
+    LOGGER.setLevel(logging.DEBUG)
+    logging.basicConfig(stream=sys.stdout)
+
+    for men in men_pipeline(validate_yml(load_yml(CONFIG_PATH))):
+        LOGGER.info(men)
