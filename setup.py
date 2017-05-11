4c957dfce7751335867c9e75afebdfbb5ef87a78
Martino Ferrari <manda.mgf@gmail.com>
2017-05-11 13:24:54 +0200

    Updated dependecies, config and gitignore

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license='MIT',
     platforms=["any"],
     packages=['mumen'],
-    install_requires=['requests', 'nltk', 'pyyaml'],
+    install_requires=['requests', 'nltk', 'pyyaml', 'lxml'],
     # Add test suite
     # Add entry point
     entry_points={
