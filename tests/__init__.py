5b7e7970d0d796bc0ef329ea8be08d2026d3e0ec
Alex Kabbach <akb@3azouz.net>
2017-05-11 22:44:21 +0200

    Removed useless __init__.py file in tests directory

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""How to write meaningful docstrings.
-
-Check out:
-http://stackoverflow.com/questions/
-2557110/what-to-put-in-a-python-module-docstring
-and
-http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
-
-"""
