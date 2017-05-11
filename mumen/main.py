7cefe7aebe68870caef2b195ead33b318ca907db
Martino Ferrari <manda.mgf@gmail.com>
2017-05-11 13:19:10 +0200

    Added load of dictionaries in main

--- mumen/main.py
+++ mumen/main.py
@@ -10,8 +10,10 @@ import logging
 from mumen.validation.validator import validate_yml
 import mumen.utils.men as men
 from mumen.utils.configloader import load_yml
-from mumen.constants import Lang, Dictionary
-from mumen.translation.translator import translate
+from mumen.constants import Lang, Dictionary, FREEDICT_DICTS
+import mumen.translation.translator as translator
+from mumen.translation.dictionaries.free_dict import FreeDict
+from mumen.translation.dictionaries.wnet_dict import WordNetDict
 from mumen.exceptions.translation import TranslationException
 from mumen.exceptions.validation import ValidationException
 
@@ -20,16 +22,27 @@ def __translation__(config):
     logger = logging.getLogger(__name__)
 
     in_file = config['source']['file']
-    # out_file = config['target']['file']
     source_lang = Lang.from_lang_id(config['source']['lang'])
     target_lang = Lang.from_lang_id(config['target']['lang'])
+    dict_path = config['jmdict_conf']['dict_dir']
+    basepath = '{}-{}'.format(source_lang.to_wnet_lang_id(),
+                              target_lang.to_wnet_lang_id())
+    basepath = FREEDICT_DICTS[basepath]
+    dict_path = '{}{}/{}.tei'.format(dict_path, basepath, basepath)
     dictionaries = []
     for entry in config['translator']:
         for name in entry:
             enabled = entry[name]
             if enabled:
-                dictionaries.append(Dictionary.from_name(name))
-    for translation in translate(
+                dic = Dictionary.from_name(name)
+                if dic == Dictionary.WORDNET:
+                    dictionaries.append(WordNetDict(source_lang, target_lang))
+                elif dic == Dictionary.FREEDICT:
+                    freedict = FreeDict(dict_path,
+                                        source_lang,
+                                        target_lang)
+                    dictionaries.append(freedict)
+    for translation in translator.translate(
             men.load(in_file),
             source_lang,
             target_lang,
