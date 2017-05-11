9eb8149f95bcc5f526ec4c0eb08f788f528fd28c
Martino Ferrari <manda.mgf@gmail.com>
2017-05-11 09:53:16 +0200

    Fixed translation step

--- mumen/translation/dictionaries/wnet_dict.py
+++ mumen/translation/dictionaries/wnet_dict.py
@@ -7,73 +7,37 @@ author: Martino Ferrari
 author_email: martino.ferrari@etu.unige.ch
 """
 from nltk.corpus import wordnet as wn
-from mumen.constants import Lang, LANGUAGES
-from mumen.exceptions.translation import TranslationException
 
 
-def lang2str(lang):
-    """Convert Lang enum to wnet string.
+class WordNetDict:
+    """WordNet based dictionary."""
+    def __init__(self, source_lang, target_lang):
+        self.__source__ = source_lang.to_wnet_lang_id()
+        self.__target__ = target_lang.to_wnet_lang_id()
 
-    Args:
-        lang: Lang enum.
+    def source(self):
+        """Get source language."""
+        return self.__source__
 
-    Returns:
-        wordnet language string.
+    def target(self):
+        """Get target language."""
+        return self.__target__
 
-    """
-    if isinstance(lang, str):
-        lang = LANGUAGES[lang]
-    if lang == Lang.JPN:
-        return "jpn"
-    if lang == Lang.ENG:
-        return "eng"
-    if lang == Lang.ARB:
-        return "arb"
-    raise Exception("Lang not supported: {}".format(lang))
+    def translate(self, word):
+        """Translate a word using WordNet.
 
+        Args:
+            word: word to translate
+        Returns:
+            Translated word
 
-def translate_word(word, lng="jpn"):
-    """Translate an English word using WordNet.
-
-    Args:
-        word: English word to translate
-        lng: target language
-    Returns:
-        Translated word
-
-    """
-    syns = wn.synsets(word)
-    if not syns:
-        raise TranslationException("No synset found for {}".format(word))
-    lemmas = []
-    for syn in syns:
-        lemmas.extend(syn.lemma_names(lng))
-    if not lemmas:
-        raise TranslationException(
-            "No translation found for {} in {}".format(word, lng))
-    return lemmas[0]
-
-
-def translate_men(men, lng="jpn"):
-    """Translate a MEN dataset using WordNet.
-
-    Args:
-        men: An english MEN dataset to translate
-        lng: target language
-    Returns:
-        Translated MEN dataset in target language
-
-    """
-    for entry in men:
-        yield {
-            "word_L": translate_word(entry["word_L"], lng),
-            "word_R": translate_word(entry["word_R"], lng),
-            "similarity": entry["similarity"]
-        }
-
-
-if __name__ == "__main__":
-    print(translate_word("cat"))
-    print(translate_word("dog"))
-    print(translate_word("sun"))
-    print(translate_word("japan"))
+        """
+        syns = wn.synsets(word)
+        if not syns:
+            return []
+        lemmas = []
+        for syn in syns:
+            for lemma in syn.lemma_names(self.__target__):
+                if lemma not in lemmas:
+                    lemmas.append(lemma)
+        return lemmas
--- mumen/translation/translator.py
+++ mumen/translation/translator.py
@@ -4,6 +4,9 @@
 author: Martino Ferrari
 author_email: martino.ferrari@etu.unige.ch
 """
+from mumen.constants import Dictionary
+from mumen.translation.dictionaries.wnet_dict import WordNetDict
+from mumen.exceptions.translation import TranslationException
 
 
 class Translator:
@@ -11,11 +14,25 @@ class Translator:
     def __init__(self, source_lang, target_lang, dictionaries):
         self.__source__ = source_lang
         self.__target__ = target_lang
-        self.__dictionaries__ = dictionaries
+        self.__dictionaries__ = []
+        self.__load__dicts__(dictionaries)
+
+    def __load__dicts__(self, dictionaries):
+        for dic in dictionaries:
+            if dic == Dictionary.WORDNET:
+                self.__dictionaries__.append(WordNetDict(
+                    self.__source__,
+                    self.__target__))
 
     def translate(self, word):
         """Translate a single word."""
-        return word
+        translation = []
+        for dic in self.__dictionaries__:
+            translation.extend(dic.translate(word))
+        if not translation:
+            raise TranslationException(
+                "No translation for the word: {}".format(word))
+        return translation[0]
 
     def translate_pairs(self, word_a, word_b):
         """Translate couple of words."""
@@ -23,8 +40,9 @@ class Translator:
 
     def translate_men_pairs(self, men_pairs):
         """Translate a list of couples of words."""
-        for (word_a, word_b) in men_pairs:
-            yield self.translate_pairs(word_a, word_b)
+        for (word_a, word_b, sim) in men_pairs:
+            trans_a, trans_b = self.translate_pairs(word_a, word_b)
+            yield (trans_a, trans_b, sim)
 
 
 def translate(men_pairs, source_lang, target_lang, dictionaries):
@@ -43,5 +61,5 @@ def translate(men_pairs, source_lang, target_lang, dictionaries):
 
     """
     translator = Translator(source_lang, target_lang, dictionaries)
-    for (trad_a, trad_b) in translator.translate_men_pairs(men_pairs):
-        yield (trad_a, trad_b)
+    for (trad_a, trad_b, sim) in translator.translate_men_pairs(men_pairs):
+        yield (trad_a, trad_b, sim)
