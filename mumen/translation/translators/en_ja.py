43b4fd6e67c03bc8a87c0acd1715138079176441
akb89 <akb@3azouz.net>
2017-05-15 08:19:25 +0200

    Added English to Japanese translator

---
+++ mumen/translation/translators/en_ja.py
@@ -0,0 +1,34 @@
+"""Translate from English to Japanese."""
+import logging
+import importlib
+from collections import defaultdict
+
+import mumen.utils.constants as const
+
+
+__all__ = ['translate']
+
+logger = logging.getLogger(__name__)
+
+
+def _translate_lemma(lemma, config, source_lang_iso_1, target_lang_iso_1):
+    translations = defaultdict(list)
+    partition = lemma.rpartition(const.HYPHEN)
+    word = partition[0]
+    pos = partition[2]
+    for dict_name, dict_config in config['dictionaries'].items():
+        if dict_config['use']:
+            dictionary = importlib.import_module(
+                'mumen.translation.dictionaries.{}'.format(dict_name))
+            translations[dictionary].extend(
+                getattr(dictionary, 'get_translations')(word,
+                                                        pos,
+                                                        source_lang_iso_1,
+                                                        target_lang_iso_1))
+    return translations
+
+
+def translate(entry):
+    """Translate the entry from English to Japanese"""
+    # TODO: detect whether entry is word or lemma (with pos)
+    return _translate_lemma(entry)
