6111f22111b9cad09ce485ce8d4effd447634a8c
akb89 <akb@3azouz.net>
2017-05-22 14:54:55 +0200

    Updated logger formatting

--- mumen/translation/dictionaries/wordnet.py
+++ mumen/translation/dictionaries/wordnet.py
@@ -13,8 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 def get_translations(lemma, target_lang_iso_1, config):
-    """Get WordNet translations of word (with pos) from source to target
-    language"""
+    """Get WordNet translations.
+
+    Get translations of word (with pos) from source to target language
+    """
     logger.debug('Loading WordNet dictionary')
     logger.debug('Translating entry: {}'.format(lemma))
     translations = []
@@ -24,7 +26,8 @@ def get_translations(lemma, target_lang_iso_1, config):
     # TODO: Add validation for languages
     source_lang_iso_2 = const.LANG_ISO_639_1_TO_2_DICT[config['from']]
     target_lang_iso_2 = const.LANG_ISO_639_1_TO_2_DICT[target_lang_iso_1]
-    synsets = wn.synsets(word, pos=const.MEN2WN_POS[pos], lang=source_lang_iso_2)
+    synsets = wn.synsets(word, pos=const.MEN2WN_POS[pos],
+                         lang=source_lang_iso_2)
     if synsets:
         logger.debug('Corresponding synsets: {}'.format(synsets))
     else:
@@ -32,8 +35,9 @@ def get_translations(lemma, target_lang_iso_1, config):
     for synset in synsets:
         wn_target_lemmas = synset.lemma_names(target_lang_iso_2)
         if wn_target_lemmas:
-            logger.debug('Corresponding target lemmas: {}'.format(wn_target_lemmas))
+            logger.debug('Corresponding target lemmas: {}'
+                         .format(wn_target_lemmas))
         else:
             logger.debug('No corresponding target lemmas found')
         translations.extend(wn_target_lemmas)
-    return ['{}-{}'.format(item, pos) for item in translations]
+    return {'{}-{}'.format(item, pos) for item in translations}
