f92e11e6489e7f4cfb505fef315287005c2cf52f
enzopoggio <poggio.enzo@gmail.com>
2017-05-28 15:38:45 +0200

    TODO repair this unuse wrapper for futur use

--- mumen/translation/dictionaries/freedict.py
+++ mumen/translation/dictionaries/freedict.py
@@ -1,10 +1,14 @@
 """Simple wrapper for FreeDict."""
 import logging
-import lxml.etree as etree
-import mumen.utils.constants as const
+# import lxml.etree as etree
+# import mumen.utils.constants as const
+
+"""
+TODO: Repair this wrapper
+"""
 
 
-__all__ = ['get_translations']
+# __all__ = ['get_translations']
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +17,8 @@ logger = logging.getLogger(__name__)
 TEI_NS = {'n': 'http://www.tei-c.org/ns/1.0'}
 
 
-def _load(dictionary_path, source_lang, target_lang):
-    """Initialize FreeDict dictionary."""
+"""def _load(dictionary_path, source_lang, target_lang):
+    # Initialize FreeDict dictionary.
     __tree__ = etree.parse(dictionary_path)
     __direct__ = '{}-{}'.format(
         source_lang.to_3_lang_id(),
@@ -22,16 +26,16 @@ def _load(dictionary_path, source_lang, target_lang):
 
 
 def get_translations(word, pos, target_lang_iso_1, config):
-    """Translate a given entry using FreeDict."""
+    # Translate a given entry using FreeDict.
     translations = []
     total = 0
-    if not self.__direct__:
+    if not __direct__:
         query = '..//n:entry[.//n:quote[text()="{}"]]'.format(word)
         sub_query = './/n:orth'
     else:
         query = '..//n:entry[.//n:orth[text()="{}"]]'.format(word)
         sub_query = './/n:quote'
-    trans = self.__tree__.xpath(query, namespaces=TEI_NS)
+    trans = __tree__.xpath(query, namespaces=TEI_NS)
     for entry in trans:
         froms = entry.xpath(sub_query, namespaces=TEI_NS)
         for trans in froms:
@@ -40,3 +44,4 @@ def get_translations(word, pos, target_lang_iso_1, config):
     for tran in translations:
         translations[tran] /= total
     return translations
+"""
